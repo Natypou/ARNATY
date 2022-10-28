73975803df209ca8c6ea7604f42703689300b3b5
Natypou <115631123+Natypou@users.noreply.github.com>
2022-10-28 11:19:04 -0300

    s

---
+++ Backend/src/test/java/com/portfolio/pou/PouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.pou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
